f962cdfdbd6e6191be68bc047f9a380516273180
toshi-toma <toshitoma1217@gmail.com>
2018-03-13 23:02:42 +0900

    Board Componentのstateを更新するために、Square ComponentにonClick関数を渡す

--- src/index.js
+++ src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null
-    };
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -27,7 +21,12 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
