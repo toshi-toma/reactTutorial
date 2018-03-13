c4871b3c077081b99c1d8cf0aea506da95a753f4
toshi-toma <toshitoma1217@gmail.com>
2018-03-13 23:27:34 +0900

    Board Componentのstateを更新するhandleClickメソッドを追加

--- src/index.js
+++ src/index.js
@@ -20,6 +20,12 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
     return (
       <Square
